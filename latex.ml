0c8799922c6b8e50b3927c5f44712943ca5f0317
jamiiecb <jamiiecb@googlemail.com>
2009-05-02 01:44:53 +0000

    Changed latex definition
    darcs-hash:20090502014453-b18dd-7330290a17fea8b801c7f5cf131c4dc60a50894b.gz

---
+++ latex.ml
@@ -0,0 +1,56 @@
+type element =
+  | Command of string * fragment
+  | Text of string
+and fragment = element list
+
+let children element =
+  match element with
+    | Command (_,fragment) -> fragment
+    | Text _ -> []
+
+(* Parsing from xml *)
+exception Bad_xml
+
+let rec element_of_xml xml =
+  match xml with
+    | Xml.Element (tag,attrs,xml) -> Command (tag, fragment_of_xml xml)
+    | Xml.PCData text -> Text text
+
+and fragment_of_xml xmls = List.map element_of_xml xmls
+
+let fragment_of_document doc =
+  match doc with
+    | Xml.Element ("document",_,contents) -> fragment_of_xml contents
+
+let parse_xml str =
+  try
+    let xml = Xml.parse_string str in
+    match xml with
+      | Xml.Element ("results",_,results) ->
+          List.map fragment_of_document results
+  with
+    | Xml.Error _ | Match_failure _ -> raise Bad_xml
+
+(* Parsing elements from json *)
+exception Bad_latex
+exception Bad_query
+
+let rec element_of_json json =
+  match json with
+    | Json_type.Object [(field,json)] -> Command (field,fragment_of_json json)
+    | Json_type.String str -> Text str
+    | _ -> raise Bad_latex
+and fragment_of_json json =
+  match json with
+    | Json_type.Array jsons -> List.map element_of_json jsons
+    | _ -> raise Bad_latex
+
+let fragment_of_query json =
+  try
+    match json with
+      | Json_type.Object fields ->
+          match List.assoc "query" fields with
+            | Json_type.Object fields ->
+                fragment_of_json (List.assoc "latex" fields)
+  with
+    | Json_type.Json_error _ | Not_found | Match_failure _ -> raise Bad_query
