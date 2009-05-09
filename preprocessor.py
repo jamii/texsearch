06744bf1d8e95858b6bc57e6e9d7393fe32228d0
jamiiecb <jamiiecb@googlemail.com>
2009-05-09 11:54:11 +0000

    Reorganised preprocessor, including bulk doc handling
    darcs-hash:20090509115411-b18dd-bb4c0c07f654de88a48d7fc3444e739dfe48aae4.gz

--- preprocessor.py
+++ preprocessor.py
@@ -1,171 +1,106 @@
 #!/bin/env python
 
 import string, re
-from plasTeX import TeXFragment, Environment
+from plasTeX import TeXFragment
 from plasTeX.DOM import Node
 
-class begin(Environment):
-  pass
+# Ignore useless tags
+ignoreSet = frozenset([
+ 'displaymath'
+,'bgroup'
+,'math'
+,'text'
+,'nulldelimiterspace'
+,'vphantom'
+,'hphantom'
+,'hfill'
+,'vfill'
+,'hbox'
+,'align'
+,'aligned'
+,'gathered'
+,'active::&'
+,'\\'
+,'#document'
+,'document'
+,'left'
+,'right'
+,'langle'
+,'rangle'])
 
-class Renderer(dict):
-    def render(self,node,output):
-        # Short circuit text nodes
-        if node.nodeType == Node.TEXT_NODE:
-            self.textDefault(node,output)
+def clean(node,output):
+  # Short circuit text nodes
+  if node.nodeType == Node.TEXT_NODE:
+    text = re.sub("\s+", "", unicode(node))
+    if text:
+      output.append(text)
+  elif node.nodeName in ignoreSet:
+    # Ignore node and move on to children
+    for child in node.childNodes:
+      clean(child,output)
+  else:
+    """ Rendering method for all non-text nodes """
+    children = []
+
+    # See if we have any attributes to clean
+    if node.hasAttributes():
+      for key, value in node.attributes.items():
+        # If the key is 'self' these nodes are the same as the child nodes
+        # If the key is '*modifier*' we dont care about it
+        if key == 'self' or key == '*modifier*':
+          continue
+        if value.__class__ is TeXFragment:
+          clean(value,children)
         else:
-          # Locate the rendering callable, and call it with the
-          # current object (i.e. `child`) as its argument.
-          func = self.find(node.nodeName, self.default)
-          func(node,output)
+          children.append(unicode(value))
 
-    def renderChildren(self, node, output):
-        # Render all child nodes
-        for child in node.childNodes:
-            self.render(child,output)
+    # Invoke cleaning on child nodes
+    for child in node.childNodes:
+      clean(child,children)
 
-    def default(self, node, output):
-        """ Rendering method for all non-text nodes """
-        children = []
-
-        # See if we have any attributes to render
-        if node.hasAttributes():
-          for key, value in node.attributes.items():
-              # If the key is 'self' these nodes are the same as the child nodes
-              # If the key is '*modifier*' we dont care about it
-              if key == 'self' or key == '*modifier*':
-                  continue
-              if value.__class__ is TeXFragment:
-                  result = self.renderChildren(value,children)
-              else:
-                  result = children.append(unicode(value))
-
-        # Invoke rendering on child nodes
-        self.renderChildren(node,children)
-
-        output.append({node.nodeName : children})
-
-    def textDefault(self, node, output):
-        """ Rendering method for all text nodes """
-        text = re.sub("\s+", "", unicode(node))
-        if text:
-          output.append(text)
-
-    def find(self, key, default=None):
-        if self.has_key(key):
-            return self[key]
-
-        self[key] = default
-        return default
-
-    def ignore(self, node, output):
-        for child in node.childNodes:
-            self.render(child,output)
-
-renderer = Renderer()
-# Get rid of some extraneous tags
-renderer['displaymath'] = renderer.ignore
-renderer['bgroup'] = renderer.ignore
-renderer['math'] = renderer.ignore
-renderer['text'] = renderer.ignore
-renderer['nulldelimiterspace'] = renderer.ignore
-renderer['vphantom'] = renderer.ignore
-renderer['hphantom'] = renderer.ignore
-renderer['hfill'] = renderer.ignore
-renderer['vfill'] = renderer.ignore
-renderer['hbox'] = renderer.ignore
-renderer['align'] = renderer.ignore
-renderer['aligned'] = renderer.ignore
-renderer['gathered'] = renderer.ignore
-renderer['active::&'] = renderer.ignore
-renderer['\\'] = renderer.ignore
-renderer['#document'] = renderer.ignore
-renderer['document'] = renderer.ignore
-renderer['left'] = renderer.ignore
-renderer['right'] = renderer.ignore
-renderer['langle'] = renderer.ignore
-renderer['rangle'] = renderer.ignore
-
-
-# Pair up left + right
+    output.append({node.nodeName : children})
 
 from plasTeX.TeX import TeX
 
-def processTeX(string):
-    # Instantiate a TeX processor and parse the input text
-    tex = TeX()
-    tex.input(string)
-    return tex.parse()
+def preprocess(string):
+  # Instantiate a TeX processor and parse the input text
+  tex = TeX()
+  tex.disableLogging()
+  tex.input(string)
+  result = []
+  clean(tex.parse(),result)
+  return result
 
-import os, sys, httplib
+import sys, httplib
 from xml.dom import minidom
 try:
-    # Python 2.6
-    import json
+  # Python 2.6
+  import json
 except:
-    # Prior to 2.6 requires simplejson
-    import simplejson as json
+  # Prior to 2.6 requires simplejson
+  import simplejson as json
+
 # Bulk process a xml document
-def processXml(fileName):
-    conn = httplib.HTTPConnection("localhost:5984")
-    headers = {"Content-type": "application/json"}
+def preprocessXml(fileName):
+  xml = minidom.parse(fileName)
+  docs = []
+  for item in xml.childNodes[0].childNodes:
+    if item.nodeName == u'result':
+      doi = item.childNodes[0].childNodes[0].wholeText
+      for node in item.childNodes[1:]:
+        source = node.childNodes[0].wholeText
+        content = preprocess("\\begin{document}"+source+"\\end{document}")
+        doc = {'doi': doi, 'source': source, 'content': content}
+        docs.append(doc)
 
-    docs = minidom.parse(fileName)
-    for item in docs.childNodes[0].childNodes:
-        if item.nodeName == u'result':
-            doi = item.childNodes[0].childNodes[0].wholeText
-            for node in item.childNodes[1:]:
-                source = node.childNodes[0].wholeText
+  conn = httplib.HTTPConnection("localhost:5984")
+  headers = {"Content-type": "application/json"}
+  conn.request("POST", "/documents/_bulk_docs", json.dumps({'all-or-nothing':True, 'docs':docs}), headers)
+  response = conn.getresponse()
+  if response.status != 201:
+    # What status codes are acceptable here?
+    raise IOError
+  conn.close()
 
-                content = []
-                renderer.render(processTeX("\\begin{document}"+source+"\\end{document}"),content)
-
-                doc = json.dumps({'doi': doi, 'source': source, 'content': content})
-                conn.request("POST", "/documents", doc, headers)
-                response = conn.getresponse()
-                if response.status != 201:
-                    # What status codes are acceptable here?
-                    raise IOError
-                response.read() # Have to read before next request
-    conn.close()
-
-def search(query):
-    conn = httplib.HTTPConnection("localhost:5984")
-    headers = {"Content-type": "application/json"}
-    conn.request("GET", "/documents/_external/index", json.dumps(query), headers)
-    response = conn.getresponse()
-    if response.status != 200:
-        # What status codes are acceptable here?
-        raise IOError
-    conn.request("POST", "documents/_all_docs?include_docs=true", {keys : response.read()}, headers)
-    response = conn.getresponse()
-    if response.status != 200:
-        # What status codes are acceptable here?
-        raise IOError
-    return response.read()
-
-def requests():
-    line = sys.stdin.readline()
-    while line:
-        yield json.loads(line)
-        line = sys.stdin.readline()
-
-def respond(request):
-    try:
-      query = request['query']
-      latex = query['latex']
-      parseResults = []
-      renderer.render(processTeX("\\begin{document}"+latex+"\\end{document}"),parseResults)
-      response = json.dumps({'json':parseResults, 'code':200}) # OK
-    except KeyError:
-      response = json.dumps({'json':{'error':'Bad request'}, 'code':400}) # Bad request
-    #except Exception, exception:
-      #response = json.dumps({'json':{'error':unicode(exception)}, 'code':500}) # Internal server error
-    sys.stdout.write("%s\n" % response)
-    sys.stdout.flush()
-
-def main():
-    for request in requests():
-        respond(request)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    preprocessXml("exs.xml")
