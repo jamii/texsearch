e75525f29434acea40def47dec5d40c61dea7857
jamiiecb <jamiiecb@googlemail.com>
2009-05-09 14:07:14 +0000

    Search proxy now works, but slowly
    darcs-hash:20090509140714-b18dd-778d235806673a92c4895fcb7074be33e9f91c7c.gz

--- search.py
+++ search.py
@@ -1,30 +1,34 @@
 #!/bin/env python
 
 import simplejson as json
-import sys, httplib
+import sys, httplib, urllib
 from preprocessor import preprocess
+import re
 
 headers = {"Content-type": "application/json"}
 
-def search(query):
+def search(latex,limit):
   conn = httplib.HTTPConnection("localhost:5984")
-  conn.request("GET", "/documents/_external/index", json.dumps(query), headers)
+  url = "/documents/_external/index?%s" % urllib.urlencode({'latex':latex, 'limit':limit})
+  conn.request("GET", url)
   response = conn.getresponse()
   if response.status != 200:
     # What status codes are acceptable here?
     raise IOError
+  result = response.read()
   conn.close()
-  return json.loads(response.read())
+  return result
 
 def lookup(ids):
   conn = httplib.HTTPConnection("localhost:5984")
-  conn.request("POST", "documents/_design/demo/_view/results", json.dumps({'keys': ids}), headers)
+  conn.request("POST", "/documents/_design/demo/_view/results", "{\"keys\": %s}" % ids, headers)
   response = conn.getresponse()
   if response.status != 200:
     # What status codes are acceptable here?
     raise IOError
+  result = response.read()
   conn.close()
-  return json.loads(response.read())
+  return result
 
 def requests():
   line = sys.stdin.readline()
@@ -32,20 +36,26 @@ def requests():
     yield json.loads(line)
     line = sys.stdin.readline()
 
+def test():
+  latex = preprocess("$$(h_b^2)$$")
+  ids = search(json.dumps(latex), 10)
+  results = re.sub("\s+", "", lookup(ids))
+  print results
+
 def main():
   for request in requests():
-    try
+    try:
       query = request['query']
-      ids = search({'latex' : preprocess(query['latex']), 'limit' : query['limit']})
-      results = lookup(ids)
+      ids = search(json.dumps(preprocess(query['latex'])), query['limit'])
+      results = re.sub("\s+", "", lookup(ids))
       code = 200 # OK
     except KeyError, e:
       results = {'error': str(e)}
       code = 400 # Bad request
-    except Exception, e:
-      results = {'error': str(e)}
-      code = 500 # Internal server error
-    sys.stdout.write("%s\n" % json.dumps({"code": code, "json": results, "headers": headers}))
+    #except Exception, e:
+      #results = {'error': str(e)}
+      #code = 500 # Internal server error
+    sys.stdout.write("{\"code\":%s, \"json\":%s}\n" % (code,results))
     sys.stdout.flush()
 
 if __name__ == "__main__":
