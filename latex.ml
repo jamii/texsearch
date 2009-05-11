20a410ecf7cadb75e7310300e9ea5c9e178b9a9d
jamiiecb <jamiiecb@googlemail.com>
2009-05-11 08:08:37 +0000

    Now hashing the entire latex element
    darcs-hash:20090511080837-b18dd-2847b4fae2757f6fd9b21c231a36fd850cc46ec6.gz

--- latex.ml
+++ latex.ml
@@ -1,7 +1,7 @@
 type element =
-  | Command of int
-  | Text of int
-and t = element array
+  | Command of string
+  | Text of string
+and t = int array
 
 (* Parsing elements from json *)
 exception Bad_latex
@@ -9,14 +9,14 @@ exception Bad_latex
 let of_json json =
   let rec element_of_json json =
     match json with
-      | Json_type.Object [(command,json)] -> (Command (Hashtbl.hash command)) :: t_of_json json
-      | Json_type.String str -> [Text (Hashtbl.hash str)]
+      | Json_type.Object [(command,json)] -> (Command command) :: t_of_json json
+      | Json_type.String text -> [Text text]
       | _ -> raise Bad_latex
   and t_of_json json =
     match json with
       | Json_type.Array jsons -> List.concat (List.map element_of_json jsons)
       | _ -> raise Bad_latex in
-  Array.of_list (t_of_json json)
+  Array.of_list (List.map Hashtbl.hash (t_of_json json))
 
 (* Defined to make json-static happy, not used *)
 let to_json latex = Json_type.Null
