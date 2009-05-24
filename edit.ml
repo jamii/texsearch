e72f96f011eef2f3ce23e57214e00cefb5576f16
jamiiecb <jamiiecb@googlemail.com>
2009-05-24 17:51:20 +0000

    Non-functional tweaks to edit distance
    darcs-hash:20090524175120-b18dd-3a1d35e4ceeb3eae84543387c3df3533e61d17e6.gz

--- edit.ml
+++ edit.ml
@@ -5,8 +5,8 @@ let rec minimum (x : int) y z =
   if y < x then minimum y x z else
   if z < y then minimum x z y else x
 
-let left_edit_distance (suffixL : Latex.t) (suffixR : Latex.t) =
-  let maxl, maxr = Array.length suffixL, Array.length suffixR in
+let left_edit_distance (latexL : Latex.t) (latexR : Latex.t) =
+  let maxl, maxr = Array.length latexL, Array.length latexR in
   if maxl = 0 then maxr else
   if maxr = 0 then maxl else
   let cache = Array.make_matrix (maxl + 1) (maxr + 1) 0 in
@@ -17,20 +17,18 @@ let left_edit_distance (suffixL : Latex.t) (suffixR : Latex.t) =
   (* General matching *)
   for l = maxl - 1 downto 1 do
     for r = maxr - 1 downto 0 do
-      let tL, tR = suffixL.(l), suffixR.(r) in
       cache.(l).(r) <-
           minimum
             (1 + cache.(l).(r+1))
             (1 + cache.(l+1).(r))
-            ((abs (compare tL tR)) + cache.(l+1).(r+1))
+            ((abs (compare latexL.(l) latexR.(r))) + cache.(l+1).(r+1))
   done done;
   (* Non-matches on the right dont count until left starts matching *)
   for r = maxr - 1 downto 0 do
-    let tL, tR = suffixL.(0), suffixR.(r) in
-      cache.(0).(r) <-
-          minimum
-            (cache.(0).(r+1))
-            (1 + cache.(1).(r))
-            ((abs (compare tL tR)) + cache.(1).(r+1))
+    cache.(0).(r) <-
+        minimum
+          (cache.(0).(r+1))
+          (1 + cache.(1).(r))
+          ((abs (compare latexL.(0) latexR.(r))) + cache.(1).(r+1))
   done;
   cache.(0).(0)
