0ca08d2fdebdf717df1ff9af4cd98cc5e4cff105
jamiiecb <jamiiecb@googlemail.com>
2009-09-27 18:11:19 +0000

    Removed the old unexpectedResponse
    Ignore-this: 3d0afbad169fad0a523f46e2b7e969e0
    
    darcs-hash:20090927181119-b18dd-a9f434b6d936739519d065d8d5709757927b7b7a.gz

--- util.py
+++ util.py
@@ -1,18 +1,3 @@
-class UnexpectedResponse(Exception):
-  def __init__(self,expected,got):
-    self.expected = expected
-    self.got = got
-
-  def __str__(self):
-    return ("Expected code %s, got code %s" % (self.expected,self.got))
-
-def expectResponse(conn,code):
-  response = conn.getresponse()
-  if response.status != code:
-    response.read() # Clear the response
-    raise UnexpectedResponse(code,response.status)
-  return response.read()
-
 def encodeDoi(doi):
   return doi.replace("/","_")
 
