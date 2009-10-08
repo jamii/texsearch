5ca14e7eae7c4af074718b29789b0067af5c2649
jamiiecb <jamiiecb@googlemail.com>
2009-10-08 00:11:07 +0000

    Refactoring to latex
    Ignore-this: 29c8cb738561f4fdff9e0b33b764afc9
    
    darcs-hash:20091008001107-b18dd-e2f0d921c25704d9c07dd60d14fa66eadb9975dd.gz

--- latex.ml
+++ latex.ml
@@ -7,7 +7,9 @@ The json input is produced by the python preprocessor.
 type element =
   | Command of string
   | Text of string
-and t = int array
+and elements = int array
+
+type t = elements
 
 let empty () = Array.create 0 0
 
@@ -17,14 +19,14 @@ exception Parse_error
 let of_json json =
   let rec element_of_json json =
     match json with
-      | Json_type.Object [(command,json)] -> (Command command) :: t_of_json json
+      | Json_type.Object [(command,json)] -> (Command command) :: elements_of_json json
       | Json_type.String text -> [Text text]
       | _ -> raise Parse_error
-  and t_of_json json =
+  and elements_of_json json =
     match json with
       | Json_type.Array jsons -> List.concat (List.map element_of_json jsons)
       | _ -> raise Parse_error in
-  Array.of_list (List.map Hashtbl.hash (t_of_json json))
+  Array.of_list (List.map Hashtbl.hash (elements_of_json json))
 
 (* Defined to make json-static happy, not used *)
 let to_json latex = Json_type.Null
