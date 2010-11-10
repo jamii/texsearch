582370b66eaf47a0737cef97036c37ecd0fef6d3
jamie <jamie@scattered-thoughts.net>
2010-11-10 19:06:37 +0000

    Pass no of items to tests
    Ignore-this: 4025084aa4a185df659b8e43a0600c5
    
    darcs-hash:20101110190637-1e580-29dadb2035f5abecada250981d56517f16fc23a6.gz

--- suffix_array_test.ml
+++ suffix_array_test.ml
@@ -11,9 +11,9 @@ let random_latex_element () =
 let random_latex max_length =
   Latex.of_array (random_array max_length random_latex_element)
 
-let test_search test search =
+let test_search test search n =
   let latex = random_latex 5 in
-  let latexs = random_list 1000 (fun () -> random_latex 1000) in
+  let latexs = random_list n (fun () -> random_latex 1000) in
   let ids = Util.range 0 (List.length latexs) in
   let items = List.combine ids latexs in
   Util.flush_line "Building...";
@@ -26,7 +26,7 @@ let test_search test search =
   let real_result = List.sort compare (search sa latex) in
   (test_result = real_result, test_result, real_result)
 
-let test_exact_search () =
+let test_exact_search n =
   let test items latex = 
     Util.filter_map 
       (fun (id,latex2) -> 
@@ -36,9 +36,9 @@ let test_exact_search () =
       items in
   let search sa latex =
     Suffix_array.find_exact sa latex in
-  test_search test search
+  test_search test search n
 
-let test_approx_search () =
+let test_approx_search n =
   let distance = Random.int 5 in
   let test items latex =
     Util.filter_map 
@@ -50,4 +50,4 @@ let test_approx_search () =
   let search sa latex =
     let results = Suffix_array.find_approx sa latex distance in
     List.map (fun (_,id) -> id) results in
-  test_search test search
+  test_search test search n
